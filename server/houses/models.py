7fdc2d341e9ed07af0428a834a914eae8dda1347
Paolo Chang <chanhwan.chang@gmail.com>
2023-01-31 23:41:32 -0500

    Create House model and set for admin panel

---
+++ server/houses/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class House(models.Model):
+
+    name = models.CharField(max_length=140)
+    price_per_night = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(
+        default=False, help_text="Does this house allow pets?")
+
+    # def __str__(self):
+    #     return self.name
