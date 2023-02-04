eb51b5c8311301bd26b335f0e6b7105a7d2b216f
Paolo Chang <chanhwan.chang@gmail.com>
2023-02-04 02:22:21 -0500

    update rooms and experiences allow to be null

--- server/wishlists/models.py
+++ server/wishlists/models.py
@@ -9,11 +9,16 @@ class Wishlist(CommonModel):
     name = models.CharField(max_length=150)
     rooms = models.ManyToManyField(
         "rooms.Room",
+        blank=True,
     )
     experiences = models.ManyToManyField(
         "experiences.Experience",
+        blank=True,
     )
     user = models.ForeignKey(
         "users.User",
         on_delete=models.CASCADE,
     )
+
+    def __str__(self) -> str:
+        return self.name
