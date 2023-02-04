9daa25a7d92b8d011d4e1392aa2ae075a00b51ed
Paolo Chang <chanhwan.chang@gmail.com>
2023-02-03 20:20:40 -0500

    Update users, rooms models

--- server/users/models.py
+++ server/users/models.py
@@ -3,7 +3,39 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=30, editable=False)
-    last_name = models.CharField(max_length=150, editable=False)
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    class LanguageChoices(models.TextChoices):
+        EN = ("en", "English")
+        KR = ("kr", "Korean")
+
+    class CurrencyChoices(models.TextChoices):
+        CAD = "cad", "Canada Dollar"
+        USD = "usd", "US Dollar"
+        WON = "won", "Korean Won"
+
+    first_name = models.CharField(
+        max_length=30,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    avatar = models.ImageField(blank=True)
     name = models.CharField(max_length=150, default="")
     is_host = models.BooleanField(default=False)
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices,
+    )
+    currency = models.CharField(
+        max_length=3,
+        choices=CurrencyChoices.choices,
+    )
