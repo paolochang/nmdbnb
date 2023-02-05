3476a093c6055f4304fb6d54261155bbdebefd15
Paolo Chang <chanhwan.chang@gmail.com>
2023-02-05 14:01:06 -0500

    add Photo and Video models

--- server/medias/models.py
+++ server/medias/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
+
+class Photo(CommonModel):
+
+    """Photo Model Definition"""
+
+    file = models.ImageField()
+    description = models.CharField(max_length=140)
+    romm = models.ForeignKey(
+        "rooms.Room",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+
+class Video(CommonModel):
+
+    """Video Model Definition"""
+
+    file = models.FileField()
+    experience = models.OneToOneField(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+    )
