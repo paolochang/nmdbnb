6e80bc8fd896bfa54cb907b807cfb79e7a57d454
Paolo Chang <chanhwan.chang@gmail.com>
2023-02-14 20:41:20 -0500

    Add Amenities and AmenityDetail API

--- server/rooms/views.py
+++ server/rooms/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
+from .models import Room, Amenity
+from .serializers import AmenitySerializer
 
 
 def see_all_rooms(request):
@@ -33,3 +38,47 @@ def see_one_room(request, room_id):
                 "not_found": True,
             },
         )
+
+
+class Amenities(APIView):
+    def get(self, request):
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(all_amenities, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            new_amenity = serializer.save()
+            serializer = AmenitySerializer(new_amenity)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class AmenityDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Amenity.objects.get(pk=pk)
+        except Amenity.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        serializer = AmenitySerializer(self.get_object(pk))
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        serializer = AmenitySerializer(
+            self.get_object(pk),
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_amenity = serializer.save()
+            return Response(AmenitySerializer(updated_amenity).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
+        return Response(status=HTTP_204_NO_CONTENT)
