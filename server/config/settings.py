0a2e38bba9dc1bcad8a896a6643ece2b72927a27
Paolo Chang <chanhwan.chang@gmail.com>
2023-02-15 02:43:39 -0500

    upload photo in dev env

--- server/config/settings.py
+++ server/config/settings.py
@@ -144,3 +144,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Auth
 
 AUTH_USER_MODEL = "users.User"
+
+# File Upload
+
+MEDIA_ROOT = "uploads"
+
+MEDIA_URL = "user-uploads/"
+
+# ENVIRONMENT VARIABLES
+
+PAGE_SIZE = 3
