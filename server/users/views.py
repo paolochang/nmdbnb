956e3fbfef9474c8cdac6c5c5a4e50a2b84a962c
Paolo Chang <chanhwan.chang@gmail.com>
2023-02-15 11:33:37 -0500

    add post method for creating new user

--- server/users/views.py
+++ server/users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ParseError
+from rest_framework import status
 from rest_framework.response import Response
 from .serializers import AuthenticatedUserSerializer
 
@@ -23,6 +24,22 @@ class Me(APIView):
         if serializer.is_valid():
             updated_user = serializer.save()
             serializer = AuthenticatedUserSerializer(updated_user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError("Password is missing")
+        serializer = AuthenticatedUserSerializer(data=request.data)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            new_user.set_password(password)
+            new_user.save()
+            serializer = AuthenticatedUserSerializer(new_user)
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
