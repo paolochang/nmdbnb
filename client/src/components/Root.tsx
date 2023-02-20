b9d6717d14947883f334a363a6b534522b3b400f
Paolo Chang <chanhwan.chang@gmail.com>
2023-02-20 17:43:08 -0500

    add app header

--- client/src/components/Root.tsx
+++ client/src/components/Root.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import { Box, Button, HStack } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { FaAirbnb } from "react-icons/fa";
 
 const Root = () => {
   return (
-    <div>
+    <Box>
+      <HStack justifyContent={"space-between"} py={4} px={8} borderBottom={1}>
+        <Box color="red.500">
+          <FaAirbnb size={28} />
+        </Box>
+        <HStack spacing={2}>
+          <Button>Log in</Button>
+          <Button colorScheme={"red"}>Sign up</Button>
+        </HStack>
+      </HStack>
       Root
       <Outlet />
-    </div>
+    </Box>
   );
 };
 
