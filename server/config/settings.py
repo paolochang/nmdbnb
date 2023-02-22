81df456c9bd2b2adfce2d06bd44fac8baaa6823a
Paolo Chang <chanhwan.chang@gmail.com>
2023-02-22 14:38:42 -0500

    add django-cors-headers to fix cors problem

--- server/config/settings.py
+++ server/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 THRID_PARTY_APPS = [
     "rest_framework",
+    "corsheaders",
 ]
 
 CUSTOM_APPS = [
@@ -61,6 +62,7 @@ INSTALLED_APPS = SYSTEM_APPS + THRID_PARTY_APPS + CUSTOM_APPS
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -161,3 +163,5 @@ REST_FRAMEWORK = {
         "config.authentication.TrustMeBroAuthentication",
     ]
 }
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
