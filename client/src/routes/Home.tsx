a1241b9eeaeec5ff822256a28429a2e654848557
Paolo Chang <chanhwan.chang@gmail.com>
2023-02-21 22:49:17 -0500

    add heart on image

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,21 +1,38 @@
-import { Box, Grid, HStack, Image, Text, VStack } from "@chakra-ui/react";
-import { FaStar } from "react-icons/fa";
+import {
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { FaStar, FaRegHeart } from "react-icons/fa";
 
 const Home = () => {
   return (
     <Grid
       pt="10"
       px="20"
-      templateColumns="repeat(5, 1fr)"
+      templateColumns="repeat(4, 1fr)"
       columnGap="4"
       rowGap="8"
     >
       <VStack alignItems="flex-start" spacing={-0.5}>
-        <Box overflow="hidden" mb="2" rounded="3xl">
+        <Box position="relative" overflow="hidden" mb="2" rounded="3xl">
           <Image
             h="280"
             src="https://a0.muscache.com/im/pictures/miso/Hosting-46695796/original/6ba850bd-6582-45a5-bce7-5b96dc6f30dd.jpeg?im_w=720"
           />
+          <Button
+            variant="unstyled"
+            position="absolute"
+            top={2}
+            right={0}
+            color="white"
+          >
+            <FaRegHeart size={20} />
+          </Button>
         </Box>
         <Box>
           <Grid templateColumns="6fr 1fr" gap="2">
